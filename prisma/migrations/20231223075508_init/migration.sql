ed44f0af8a0b78f9a45a8c2bddd5672a864f6d92
MrEpoch <alexandrstencuk@seznam.cz>
2023-12-23 17:48:10 +0100

    Some auth changes

---
+++ prisma/migrations/20231223075508_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "supabaseUserId" TEXT NOT NULL,
+    "courses" TEXT[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_supabaseUserId_key" ON "User"("supabaseUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_courses_key" ON "User"("courses");
--- src/routes/(auth)/verify-email/+page.svelte
+++ src/routes/(auth)/verify-email/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+  import emailVerify from "assets/email-verify.svg";
+</script>
+
 <div class="min-h-screen w-full">
+  <div class="max-w-screen-xl w-full mx-auto p-4 flex flex-col gap-5">
+    <h1 class="text-3xl font-bold">Verify your Email</h1>
+    <p>For verification, please check your email.</p>
+    <img src={emailVerify} alt="Email Verification" />
+    <p>
+      We've sent you an email with a link to verify your email. If you don't 
+    </p>
+  </div>
 </div>
